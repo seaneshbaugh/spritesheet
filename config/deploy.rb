fe1d676b0c683ea2ae0d717fe14b5d314d450e7d
Sean Eshbaugh <seaneshbaugh@gmail.com>
2014-04-04 13:51:16 -0500

    Added real restart Capistrano task.

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,3 +15,11 @@ set :bundle_flags, '--deployment'
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+
+namespace :deploy do
+  desc 'No, for real, restart.'
+  task :restart_for_real do
+    run "touch #{release_path}/tmp/restart.txt"
+  end
+  after 'deploy:restart', 'deploy:restart_for_real'
+end
