ee92e5ac6e3624c1de6f3f076ca3c29b86b1bf8a
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-09-27 18:27:25 -0500

    Removed Capistrano stuff.

--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-server 'spritesheet.seaneshbaugh.com', :app, :web, :primary => true
-
-set :domain, 'spritesheet.seaneshbaugh.com'
-set :branch, 'master'
-set :rack_env, 'production'
-set :deploy_to, "/home/#{user}/#{application}/#{rack_env}"
