ef97730a05960e8f4613804ba9957b4145745b10
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-10-09 17:16:59 -0500

    Updated webpack config to handle react.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -35,12 +35,12 @@ const common = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: [node_modules_dir],
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"]
+            presets: ["@babel/preset-env", "@babel/preset-react"]
           }
         }
       },
